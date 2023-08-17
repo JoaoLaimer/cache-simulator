f0cfbd297c04ad38958a865474a00070ddfe63d2
JoaoLaimer <jvlaimer@gmail.com>
2023-08-16 22:35:19 -0300

    added adresses

--- cache_simulator.py
+++ cache_simulator.py
@@ -1,8 +1,7 @@
-
 import sys
 
 def main():
-	if (len(sys.argv) != 7):
+	if (len(sys.argv) != 6):
 		print("Numero de argumentos incorreto. Utilize:")
 		print("python cache_simulator.py <nsets> <bsize> <assoc> <substituição> <flag_saida> arquivo_de_entrada")
 		exit(1)
@@ -12,21 +11,43 @@ def main():
 	assoc = int(sys.argv[3])
 	subst = sys.argv[4]
 	flagOut = int(sys.argv[5])
-	arquivoEntrada = sys.argv[6]
+	#arquivoEntrada = sys.argv[6]
 
+	class Cache:
 
+		def __init__(self,nsets,bsize,assoc,subst,flagOut):
+			self.nsets = nsets
+			self.bsize = bsize
+			self.assoc = assoc
+			self.subst = subst
+			self.flagOut = flagOut
+			self.way = []
+			#self.arquivoEntrada = arquivoEntrada
 
-	print("nsets =", nsets)
-	print("bsize =", bsize)
-	print("assoc =", assoc)
-	print("subst =", subst)
-	print("flagOut =", flagOut)
-	print("arquivo =", arquivoEntrada)
+		def print_atributes(self):
+			print("nsets =", self.nsets)
+			print("bsize =", self.bsize)
+			print("assoc =", self.assoc)
+			print("assoc1 =", len(self.sets))
+			print("subst =", self.subst)
+			print("flagOut =", self.flagOut)
+			for i in range(self.nsets):
+				self.sets[i].print_atributes()
+			#print("arquivo =", self.arquivoEntrada)
 
+		def create_cache(self):
+			print("Criando cache")
+			for i in range(self.assoc):
+				self.way.append(Cache_way(self.nsets,self.bsize,self.assoc))
+			print("Cache criada")
 
-	# Seu codigo vai aqui
+	class Cache_way:
+		def __init__(self):
+			self.blocks = []	
 
-
+		
+	C = Cache(nsets,bsize,assoc,subst,flagOut)
+	C.print_atributes()
 
 if __name__ == '__main__':
 	main()
